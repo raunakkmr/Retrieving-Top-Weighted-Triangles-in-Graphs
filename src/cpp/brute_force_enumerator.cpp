b853c94f35aeecc833457d39c2b3860746903c58
Raunak Kumar <raunak.kumar17@outlook.com>
2019-07-24 12:53:27 -0400

    binary writes in brute force enumerator

--- src/cpp/brute_force_enumerator.cpp
+++ src/cpp/brute_force_enumerator.cpp
@@ -19,20 +19,52 @@ int main(int argc, char* argv[]) {
     auto all_tris = brute_force_sampler(G);
     int num_all_tris = all_tris.size();
 
+    auto get_bytes = [](int x) {
+      if (x <= numeric_limits<char>::max()) {
+        return 0;
+      } else if (x <= numeric_limits<short>::max()) {
+        return 1;
+      } else if (x <= numeric_limits<int>::max()) {
+        return 3;
+      }
+      return -1;
+    };
+
     // ofstream triangle_file(dataset+"-triangles.txt");
     ofstream triangle_file(dataset+"-triangles.binary", ios::binary);
     binary_write(triangle_file, num_all_tris);
+    int est_bytes = 0;
     map<long long, long long> num_tris;
     for (auto t : all_tris) {
-      // triangle_file << t.weight << get<0>(t.vertices) << " " << get<1>(t.vertices) << " " << get<2>(t.vertices) << '\n';
       int w = t.weight;  // TODO: change to long long.
       int a = get<0>(t.vertices);
       int b = get<1>(t.vertices);
       int c = get<2>(t.vertices);
-      binary_write(triangle_file, w);
-      binary_write(triangle_file, a);
-      binary_write(triangle_file, b);
-      binary_write(triangle_file, c);
+      // binary_write(triangle_file, w);
+      // binary_write(triangle_file, a);
+      // binary_write(triangle_file, b);
+      // binary_write(triangle_file, c);
+      int bytes = 0;
+      // uses 2 as a special code that the weight is 1.
+      // call it premature optimization but it saves a lot
+      bytes |= (w == 3 ? 2 : get_bytes(w));
+      bytes <<= 2;
+      bytes |= get_bytes(a);
+      bytes <<= 2;
+      bytes |= get_bytes(b);
+      bytes <<= 2;
+      bytes |= get_bytes(c);
+
+      binary_compressed_write(triangle_file, bytes);
+      binary_compressed_write(triangle_file, a);
+      binary_compressed_write(triangle_file, b);
+      binary_compressed_write(triangle_file, c);
+      est_bytes += 4 + get_bytes(a) + get_bytes(b) + get_bytes(c);
+      if (w > 3) {
+        binary_compressed_write(triangle_file, w);
+        est_bytes += 1 + get_bytes(w);
+      }
+
       num_tris[get<0>(t.vertices)]++;
       num_tris[get<1>(t.vertices)]++;
       num_tris[get<2>(t.vertices)]++;
