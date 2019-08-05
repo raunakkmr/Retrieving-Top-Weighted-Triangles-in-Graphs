281606f5a40537ea5097b285fa11eb87e7d905c1
Raunak Kumar <raunak.kumar17@outlook.com>
2019-08-05 15:11:13 +0800

    added scripts

---
+++ scripts/compare_deterministic_1000.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+declare -a datasets=(
+    congress-bills
+    tags-stack-overflow
+    threads-math-sx
+    threads-stack-overflow
+    wikipedia
+    eth
+    aminer
+    MAG
+)
+make -C ../src/cpp clean
+make -C ../src/cpp compare_deterministic
+mkdir ../output/
+mkdir ../output/compare_deterministic_1000
+for i in "${!datasets[@]}"
+do
+    dataset=${datasets[$i]}
+    ../src/cpp/compare_deterministic ../data/binaries/${dataset}.binary ../output/brute_force_enumerator/${dataset}-triangles.binary 1 1000 &> ../output/compare_deterministic_1000/${dataset}
+done
+../src/cpp/compare_deterministic /mnt/disks/additional_ssd_dir/spotify.binary notusingrightnow 1 1000 &> ../output/compare_deterministic_1000/spotify
