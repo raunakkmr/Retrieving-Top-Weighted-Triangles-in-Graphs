9915778fa35a4c09004742b31bf5b85eec35d47d
Raunak Kumar <raunak.kumar17@outlook.com>
2019-09-09 14:32:24 +0000

    scripts need to be updated, removed older ones for now

--- scripts/compare_parallel_edge.sh
+++ scripts/compare_parallel_edge.sh
@@ -34,27 +34,27 @@ do
     do
         for dataset in "${datasets[@]}"
         do
-            ../src/cpp/compare_parallel_sampling -filename=../data/binaries/${dataset}.binary -binary=true -k=${k} -sampler=edge -start_time=0.4 -end-time=2.0 -increment=0.4 &> ../output/compare_parallel_edge_${k}_${run}/${dataset}
+            ../src/cpp/compare_parallel_sampling -filename=../data/binaries/${dataset}.binary -binary=true -k=${k} -sampler=edge -start_time=0.4 -end_time=2.0 -increment=0.4 &> ../output/compare_parallel_edge_${k}_${run}/${dataset}
         done
     done
 done
 
 for run in {1..10..1}
 do
-    ../src/cpp/compare_parallel_sampling -filename=../data/binaries/temporal-reddit-reply.binary -binary=true -k=1000 -sampler=edge -start_time=1.5 -end-time=3.5 -increment=0.5 &> ../output/compare_parallel_edge_1000_${run}/temporal-reddit-reply
+    ../src/cpp/compare_parallel_sampling -filename=../data/binaries/temporal-reddit-reply.binary -binary=true -k=1000 -sampler=edge -start_time=1.5 -end_time=3.5 -increment=0.5 &> ../output/compare_parallel_edge_1000_${run}/temporal-reddit-reply
 done
 
 for run in {1..10..1}
 do
-    ../src/cpp/compare_parallel_sampling -filename=../data/binaries/temporal-reddit-reply.binary -binary=true -k=100000 -sampler=edge -start_time=2.0 -end-time=6.0 -increment=1.0 &> ../output/compare_parallel_edge_100000_${run}/temporal-reddit-reply
+    ../src/cpp/compare_parallel_sampling -filename=../data/binaries/temporal-reddit-reply.binary -binary=true -k=100000 -sampler=edge -start_time=2.0 -end_time=6.0 -increment=1.0 &> ../output/compare_parallel_edge_100000_${run}/temporal-reddit-reply
 done
 
 for run in {1..10..1}
 do
-    ../src/cpp/compare_parallel_sampling -filename=../data/binaries/spotify.binary -binary=true -k=1000 -sampler=edge -start_time=30 -end-time=30 -increment=30 &> ../output/compare_parallel_edge_1000_${run}/spotify
+    ../src/cpp/compare_parallel_sampling -filename=/mnt/disks/additional_ssd_dir/spotify.binary -binary=true -k=1000 -sampler=edge -start_time=30 -end_time=30 -increment=30 &> ../output/compare_parallel_edge_1000_${run}/spotify
 done
 
 for run in {1..10..1}
 do
-    ../src/cpp/compare_parallel_sampling -filename=../data/binaries/spotify.binary -binary=true -k=100000 -sampler=edge -start_time=20 -end-time=20 -increment=20 &> ../output/compare_parallel_edge_100000_${run}/spotify
+    ../src/cpp/compare_parallel_sampling -filename=/mnt/disks/additional_ssd_dir/spotify.binary -binary=true -k=100000 -sampler=edge -start_time=20 -end_time=20 -increment=20 &> ../output/compare_parallel_edge_100000_${run}/spotify
 done
